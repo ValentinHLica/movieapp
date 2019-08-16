51da7a16211d7f1dfc257cce88e52ef4eee2ddc6
bashvtini <bashvtini@gmail.com>
2019-08-16 13:14:26 +0200

    Updates

---
+++ precache-manifest.7319e263ca620e65444e206b2ae44c81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60fe35979683e50d376d5b3d1454973",
+    "url": "/movieapp/index.html"
+  },
+  {
+    "revision": "63d78a95aa85a7cd6d2b",
+    "url": "/movieapp/static/css/main.93492f1b.chunk.css"
+  },
+  {
+    "revision": "07d6c7d7528f7e16af8f",
+    "url": "/movieapp/static/js/2.bc31e0df.chunk.js"
+  },
+  {
+    "revision": "63d78a95aa85a7cd6d2b",
+    "url": "/movieapp/static/js/main.b233c410.chunk.js"
+  },
+  {
+    "revision": "4e19d316896026f2633d",
+    "url": "/movieapp/static/js/runtime~main.839b6e54.js"
+  },
+  {
+    "revision": "3125284f9d27b86c4565d139f3650b83",
+    "url": "/movieapp/static/media/errr.3125284f.png"
+  }
+]);
